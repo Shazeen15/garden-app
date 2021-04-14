33786909a79c3dd2da6a04edda6bdc059847d203
Shazeen Fabius <shazeenfabius@Shazeens-MBP.lan>
2021-04-14 10:41:22 -0700

    cleaning up some code

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,15 +4,11 @@ import { setUser } from "./../utils/actions/userAction";
 import { connect } from "react-redux";
 function Home(props) {
   const { user, setUser } = props;
-  const changeUser = () => {
-    setUser("Jamile");
-  };
   return (
     <section className="home">
       <h1>Welcome {user.userName}</h1>
       <Link to="/plant">View All Your Plants</Link>
       <Link to="/add-plant">Add A New Plant</Link>
-      <button onClick={changeUser}>Change User</button>
     </section>
   );
 }
